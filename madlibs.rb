4e8e68c50f582a797797e4ad8a29126118c6f4ef
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 17:35:16 -0700

    madlibs added

---
+++ madlibs.rb
@@ -0,0 +1,16 @@
+# Run "ruby madlibs.rb" in your terminal for your game to start
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity name: "
+celebrity = gets.chomp()
+
+
+
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
+
+
