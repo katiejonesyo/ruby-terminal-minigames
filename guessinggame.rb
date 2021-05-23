c02d7539f71fdb0f492ebab62ad7f8e3438f80d3
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 23:36:19 -0700

    guessing game!

---
+++ guessinggame.rb
@@ -0,0 +1,25 @@
+# Guess the word! 3 strikes and youre out!
+# Run "ruby guessinggame.rb" in your terminal for your game to start
+
+secret_word = "hippo"
+guess =""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+while guess != secret_word and !out_of_guesses
+    if guess_count < guess_limit
+        puts "Enter your guess: "
+        guess = gets.chomp()
+        guess_count += 1
+    else
+        out_of_guesses = true
+    end
+end
+
+if out_of_guesses
+    puts "You Lose"
+else
+    puts "You won!"
+end
+
